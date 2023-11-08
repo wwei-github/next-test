e43e12b97bef1b3202617ce9098dc2707adfbc9b
wwei-github <418047394@qq.com>
2023-11-08 12:12:56 +0800

    feat: return val

--- src/app/api/getDatabase/route.ts
+++ src/app/api/getDatabase/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 
 import { Client } from "@notionhq/client";
+import { NextApiRequest, NextApiResponse } from "next";
 const notion = new Client({
   auth: "secret_ET5vUoXz7Pz9fuwlSD6kd6S5xxvN4qFsxQoSW5ZzIqI",
 });
 
-export const GET = async (req: Request, res: Response) => {
+export const GET = async (req, res: NextResponse) => {
   const response = await notion.databases.query({
     database_id: "d91f344d-4cff-46fa-a72c-59994828d64b",
     // filter: {
@@ -33,28 +34,28 @@ export const GET = async (req: Request, res: Response) => {
   });
 
   const data = response.results.map((item) => transformer(item));
-  return NextResponse.json({ data });
+  console.log(data);
+  return NextResponse.json(data);
 };
+ function transformer(page: any) {
+   let data: any = {};
+   for (const key in page.properties) {
+     switch (page.properties[key].type) {
+       case "relation":
+         data[key] = page.properties[key].relation[0].id;
+         break;
 
-async function transformer(page: any) {
-  let data: any = {};
-  for (const key in page.properties) {
-    switch (page.properties[key].type) {
-      case "relation":
-        data[key] = page.properties[key].relation[0].id;
-        break;
+       case "title":
+       case "rich_text":
+         data[key] =
+           page.properties[key][page.properties[key].type][0].text.content;
+         break;
 
-      case "title":
-      case "rich_text":
-        data[key] =
-          page.properties[key][page.properties[key].type][0].text.content;
-        break;
-
-      default:
-        console.log(key, page.properties);
-        data[key] = page.properties[key][page.properties[key].type];
-        break;
-    }
-  }
-  return data;
-}
+       default:
+         console.log(key, page.properties);
+         data[key] = page.properties[key][page.properties[key].type];
+         break;
+     }
+   }
+   return data;
+ }
